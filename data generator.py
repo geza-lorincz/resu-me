8318851fb7fe46aee16efcef3e75595ccf73b3f5
geza-lorincz <45147619+geza-lorincz@users.noreply.github.com>
2023-01-17 20:32:37 -0500

    Data generator improved and new data added&labeled

--- data generator.py
+++ data generator.py
@@ -2,8 +2,12 @@ from nltk import sent_tokenize
 
 # input raw text to be converted
 raw_text = "Required Skills: 2-4 years of programming experience required! Strong desire to learn and grow required! Previous professional experience in Java mandate. Previous 1-3 years' experience in SQL required. Must be open to relocation after the training. What We Offer: Rapid career growth, with focused and intensive training. Paid training for 8 weeks. Compensation in line with industry forms. Excellent medical, dental, and vision benefits. Relocation assistance, if required for client assignment. Mentorship and support throughout the program. Networking opportunities within your industry with peers and industry leader. About Pyramid Consulting: GenSpark is a division of Pyramid Consulting, a $310M IT Consulting firm. Pyramid Consulting is among the Top 100 largest minority and privately owned IT Consulting firms in the U.S.. The success of our clients is facilitated through our ability to provide full-spectrum support via our development centers – from a single consultant under their management, at their site, to full turnkey solutions onsite and offshore."
-punctuation = ['!', '"', '$', '%', '&', ',', '(', ')', '*', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', '"']
+punctuation = ['!', '–', '"', '$', '%', '&', ',', '(', ')', '*', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', '"']
 
+with open('text_import.txt', 'r') as f:
+    raw_text = f.read()
+
+print(raw_text)
 
 
 # split text by sentences and words
@@ -18,7 +22,7 @@ for sent in text:
             new_word = new_word[:-1]
         if new_word[0] not in punctuation:
             new_sent = new_sent + new_word + " "
-            print(new_sent + ",0")
+            print(new_sent[:-1] + ",0")
     if len(new_sent) > 0:
         preprocessed_text.append(new_sent)
 
